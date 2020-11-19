271b60224e9300cdb54ce84148f4981ea4d53c23
Petar Cale <usavatreni@gmail.com>
2020-11-19 03:39:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-//create variable for empty array
-var resultArray = [];
-
 //use for loop which iterates over the array of musicians
 //also loop throuh instruments
 //set a variable result to hold string
 //push string into the result array
 //then return resultArray
-function theBeatlesPlay(musicians, instruments) {
-  for (var i = 0; i < musicians.length; i++) {
-    for(var j = 0; j < instruments.length; j++) {
-      var result = musicians[i] + " plays " + instruments[j]
-      resultArray.push(result)
-    }
-    return resultArray;
-  }
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+//create variable for empty array
+var resultArray = [];
 
+function theBeatlesPlay(musicians, instruments) {
+  for (var i = 0; i <musicians.length; i++) {
+    resultArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return resultArray
 }
