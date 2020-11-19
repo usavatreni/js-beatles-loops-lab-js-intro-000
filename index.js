0e938848b1a69e7035c0566acbe9f724a4e82b49
Petar Cale <usavatreni@gmail.com>
2020-11-19 03:51:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,12 @@ function johnLennonFacts(array) {
   }
   return array;
 }
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++
+  } while (number < 15);
+  return array;
+}
